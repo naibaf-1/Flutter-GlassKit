62892ca239a4b3481b3a99188eda8a3a0f79555d
naibaf-1 <175958349+naibaf-1@users.noreply.github.com>
2026-02-27 16:58:08 +0100

    Simplified IconButton

--- lib/icon_glass_button.dart
+++ lib/icon_glass_button.dart
@@ -1,6 +1,5 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:flutter_glasskit/border_painter.dart';
+import 'package:flutter_glasskit/base_glass_button.dart';
 
 class IconGlassButton extends StatelessWidget {
   final VoidCallback? onTap;
@@ -10,14 +9,14 @@ class IconGlassButton extends StatelessWidget {
   final Color gradientStart;
   final Color gradientEnd;
   final double opacity;
-  final Widget icon;
+  final IconData icon;
 
   const IconGlassButton({
     super.key,
     required this.backgroundColor,
-    required this.icon,
     required this.gradientStart,
     required this.gradientEnd,
+    required this.icon,
     this.opacity = 0.5,
     this.splashColor,
     this.hoverColor,
@@ -26,37 +25,15 @@ class IconGlassButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 48,
-      width: 48,
-      child: CustomPaint(
-        // Draw the border
-        painter: BorderPainter(
-          gradient: LinearGradient(
-            begin: AlignmentGeometry.xy(0.5, 0.25),
-            end: AlignmentGeometry.xy(0, 0),
-            colors: [gradientEnd, gradientStart]
-          ),),
-        // Display the content above everything else
-        child: Padding(
-          padding: const EdgeInsets.all(2.0),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(12),
-            child: BackdropFilter(
-              filter: ImageFilter.blur(sigmaX: 12, sigmaY: 12),
-              child: Material(
-                color: backgroundColor.withOpacity(opacity),
-                child: InkWell(
-                  onTap: onTap,
-                  splashColor: splashColor,
-                  hoverColor: hoverColor,
-                  child: Center(child: icon),
-                ),
-              ),
-            ),
-          ),
-        ),
-      ),
+    return BaseGlassButton(
+      backgroundColor: backgroundColor,
+      icon: Icon(icon),
+      gradientStart: gradientStart,
+      gradientEnd: gradientEnd,
+      opacity: opacity,
+      splashColor: splashColor,
+      hoverColor: hoverColor,
+      onTap: onTap,
     );
   }
 }
